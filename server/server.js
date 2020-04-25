e1778aee600158930e96f8264564a51c443a23a4
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-04-25 17:48:29 +0400

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+
+const users = require('./routes/api/users')
 
 const app = express()
 
@@ -19,6 +22,15 @@ const db = require('./config/keys').mongoURI
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 .then( () => console.log("MongoDB successfully connected"))
 .catch( err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require('./config/password')(passport)
+
+// Routes
+app.use('/api/users', users)
 
 const port = process.env.PORT || 5000 // process.env.port is Heroku port
 
