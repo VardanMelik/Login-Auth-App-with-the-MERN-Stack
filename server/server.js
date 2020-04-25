db6eba380f98ba243eeb047164784013368ed8d8
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-04-25 16:02:07 +0400

    upload

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// Body Parser middileware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+app.use(bodyParser.json())
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then( () => console.log("MongoDB successfully connected"))
+.catch( err => console.log(err))
+
+const port = process.env.PORT || 5000 // process.env.port is Heroku port
+
+app.listen( port, () => console.log(`Server up and running on port ${port} !`))
